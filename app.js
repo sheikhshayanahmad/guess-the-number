327c8ae1db4a0beff97a72b9a61dd2f04baec135
xmortalop <64434119+xmortalop@users.noreply.github.com>
2022-01-26 01:04:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,504 @@
+
+// /* console.log('hello');
+
+// const name = 'john doe';
+// console.log(typeof name);
+
+// const today = new Date();
+// console.log(today);
+// console.log(typeof today);
+
+// let val;
+// //no of string
+// val = String(24);
+// val = String(4+4);
+// //bool to string
+// val = String(false);
+// //date to string
+
+// val = String(new Date());
+// //array  to string
+// val = String([1,2,3,5,6]);
+
+// //toString()
+// val =(4).toString();
+// val =(true).toString();
+
+
+// //output
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+
+// //string to no
+
+// val =Number('5');
+
+// //output
+//  console.log(val);
+//  console.log(typeof val);
+//  //console.log(val.length)
+//  console.log(val.toFixed(3));
+
+// val = parseInt('100.30');
+// console.log(val);
+
+// /*math object
+// val =Math.PI;
+// console.log(val);
+// val = Math.E;
+// console.log(val);
+// val = Math.round(2.3);
+// console.log(val);
+// val = Math.ceil(2.4);
+// console.log(val);
+// val = Math.floor(2.8);
+// console.log(val);
+// val = Math.sqrt(81);
+// console.log(val);
+// val = Math.pow(8,3);
+// console.log(val);
+// val = Math.min(2,6,5,1,-4,-2);
+// console.log(val);
+// val = Math.max(2,6,5,1,-4,-2);
+// console.log(val);
+// val = Math.random();
+// console.log(val);
+// val = Math.floor(Math.random() * 20 + 2);
+// console.log(val);
+
+
+
+// //STRING CONCENTINATION
+//  const firstName = 'shanu';
+//  const lastName ='ahmed';
+//  const age = 22;
+  
+//  let val;
+
+//  val = firstName + lastName ;
+//  val = firstName + ' ' + lastName ;
+
+//  console.log(val);
+//  //append
+//  val ='shanu ';
+//  val += 'web';
+//  val = 'hello i am' + ' ' + firstName + ' ' + 'and i am' + ' '+ age;
+//  console.log(val);
+ 
+// */
+//  //create some arrrays 
+
+// //  const number = [22,34,55,66,21,4,5];
+// //  const number2 = new Array(45,22,12,1,5,6,22);
+// //  const FRUIT  = ['bpple' , 'anan', 'pear'];
+// //  const mixed = [15,'shanu',true,undefined,null,{a:1,b:2},new Date()];
+// // /*
+// //  console.log(mixed);
+
+// //  let val;
+// //   console.log(number);
+// //  //get array length 
+// //  val = number.length;
+// //  console.log(val);
+
+// //  //check if array 
+// //  val = Array.isArray(number);
+// //  console.log(val);
+
+// //  //get a single value 
+// //   val =  number[0];
+// //   val = number[3];
+// //   console.log(val);
+// // //insert into array
+// //   number[4]=95;
+// //   console.log(number);
+
+//   // val = number.indexOf(34);
+//   // console.log(val);
+
+//   // //mutating arrays 
+//   // //add to end 
+//   // number.push(60);
+//   // console.log(number);
+//   // //add to front
+//   // number.unshift(57);
+//   // console.log(number);
+//   // //remove from end
+//   // number.pop();
+//   // console.log(number);
+//   // //remove from start 
+//   // number.shift();
+//   // console.log(number);
+//   // //splice value
+//   // number.splice(1,2);
+//   // console.log(number);
+
+//   ///reverse
+// //   number.reverse();
+
+// //   val =number.concat(number2);
+// //   console.log(val);
+
+// //   //sorting array
+// //   val =FRUIT.sort();
+// //   console.log(val);
+// //   val =number.sort();
+// //   console.log(val);
+  
+// //   //object literals 
+
+// //   const person = {
+// //       firstname :'shanu',
+// //       age:22,
+// //       hobbies :['music','cricket'],
+// //       address :{
+
+// //         city:'kaachi',
+// //         state : 'fl'
+// //       },
+// //       getbirthyear: function(){
+// //           return 2022-this.age;
+          
+// //       }
+// //   }
+// //   let val;
+// //   val =person.firstname;
+// //   val =person.age;
+// //   val =person.hobbies[0];
+// //   val =person.address.state;
+// //    console.log(val);
+// //   val =person.getbirthyear();
+// //   console.log(val);
+// //   */
+// // // let val;
+// // // const today= new Date();
+// // // let birthday= new Date('10-1-2022 11:30:05');
+// // // birthday =new Date('September 10 2017');
+
+// // // val =today.getFullYear();
+// // // val =today.getHours();
+
+// // // console.log(val);
+
+// // // const id = 100;
+// // // if(id == 100)
+// // // {
+// // //     console.log('correct');
+
+// // // }
+// // // else {
+// // //     console.log('incorrect');
+// // // }
+
+
+// // // if(id != 100)
+// // // {
+// // //     console.log('correct');
+
+// // // }
+// // // else {
+// // //     console.log('incorrect');
+// // // }
+
+// // //   const idd = '100';
+// // // if(idd != 100)
+// // // {
+// // //     console.log('correct');
+
+// // // }
+// // // else {
+// // //     console.log('incorrect');
+// // // }
+
+
+// // // if(id === 100)
+// // // {
+// // //     console.log('correct');
+
+// // // }
+// // // else {
+// // //     console.log('incorrect');
+// // // }
+
+// // // //logivsl operators
+// // // //and
+// // // const name = 'shanu';
+// // // const age = 15;
+
+// // // if (age > 0 &&  age< 12){
+// // // console.log('${name} is a child');
+
+// // // }else if (age >= 13 && age <= 19){
+// // // console.log(name + 's');
+// // // }
+// // // else {
+// // //     console.log('${name}is an adult');
+// // // }
+
+// // // //or
+
+
+// // // if (age > 0 || age< 12){
+// // // console.log('${9name} is a child');
+
+// // // }else if (age >= 13 || age <= 19){
+// // // console.log(name + 's');
+// // // }
+// // // else {
+// // //     console.log('${name}is an adult');
+// // // }
+
+// // // //ternary operator 
+// // // console.log(id === 100 ? 'correct' : 'incorrect');
+
+
+// // // //for loop
+
+// // // for(let i=0; i<=10; i++){
+// // //     if(i===2){
+// // //         console.log('2 is mu');
+// // //     }
+// // //     console.log('num ' + i);
+
+   
+// // // }
+// // // const cars =['ford','shcy','honda','toyota']
+// // //     for(let i=0; i< cars.length; i++){
+// // //         console.log(cars[i]);
+// // //     }
+// // // //foreach
+// // //     cars.forEach(function(cars) {
+// // //         console.log(cars);
+        
+// // //     });
+    
+
+// // //     //window method/object/properties
+
+// // // //     window.alert('hello wordld');
+// // // // alert('hello wordld');
+
+// // // //prompt
+ 
+// // // const input = prompt();
+// // // alert('hello wordld');
+
+// // // //confirm
+// // // if(confirm('are you sure')){
+// // //     console.log('yes');SSSS
+
+
+// // // } else {
+// // //     console.log('no');
+// // // }
+
+
+// // // ouer height NS WIDTH 
+
+// // // let val;
+// // // val=window.outerHeight;
+// // // console.log(val);
+// // // val=window.scrollX;
+// // // console.log(val);
+
+// // // //location object 
+
+// // // val= window.location;
+// // // // val= window.location.hostname;
+// // // // val= window.location.port;
+// // // console.log(val);
+// // // //redirect
+// // // window.location.href = 'http://google.com';
+// // // console.log(val);
+// // // //reload
+// // // window.location.reload();
+// // // console.log(val);
+
+// // //hisory object 
+// // // window.history.go(-2)
+// // // val = window.history.length;
+
+// // //naivigator object 
+// // // v
+
+// // // let val;
+
+// // // val = document;
+// // // console.log(val);
+// // // val = document.all.length;
+// // // console.log(val);
+// // // val =document.domain;
+// // // console.log(val);
+
+// // // val =document.URL;
+// // // console.log(val);
+// // // val =document.characterSet;
+// // // console.log(val);
+
+// // //document.getelement by id
+// // // console.log(document.getElementById('task-title'));.
+
+// // // // get thing from elemnet 
+
+// // // console.log(document.getElementById('task-title').id);
+// // // console.log(document.getElementById('task-title').className);
+
+// // // //change styling 
+// // // document.getElementById('task-title').style.background ='#333';
+// // // document.getElementById('task-title').style.color ='#333';
+ 
+// // // // change content
+
+// // // document.getElementById('task-title').textContent='task List';
+// // // document.getElementById('task-title').innerText=' My task List';
+// // // document.getElementById('task-title').innerHTML='<span style ="color:red"> task list </span>';
+
+// // // const taskTitle = document.getElementById('task-title');
+
+// // // //change styling 
+// // // taskTitle.style.background ='#333';
+// // // taskTitle.style.color ='#333';
+ 
+// // // // change content
+
+// // // taskTitle.textContent='task List';
+// // // taskTitle.innerText=' My task List';
+// // // taskTitle.innerHTML='<span style ="color:red"> task list </span>';
+
+// // // //document.queryseclector()
+
+// // // console.log(document.querySelector('#task-title'));
+// // // console.log(document.querySelector('.card-title'));
+// // // console.log(document.querySelector('h5'));
+
+// // // document.querySelector('li').style.color ='red';
+// // // document.querySelector('li:last-child').style.color ='red';
+// // // document.querySelector('li:nth-child(3)').style.color ='red';
+// // // document.querySelector('li:nth-child(4)').style.color ='red';
+// // // document.querySelector('li:nth-child(odd)').style.color ='red';
+// // // document.querySelector('li:nth-child(even)').style.color ='red';
+
+
+// // //document.getelementsbyclass name 
+
+// // const items =document.getElementsByClassName('header');
+// // console.log(items);
+
+// //create element 
+// const li=document.createElement('li');
+
+// // add class
+// li.className='collection-list';
+// console.log(li);
+
+//game value
+let min=1,
+    max=10,
+    winningNUM =getRandomNUM( max  ,min),
+    guessleft=3;
+
+    //ui element
+    const game=document.querySelector('#game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessbtn = document.querySelector('#guess-btn'),
+    guessInput  = document.querySelector('#guess-input'),
+   message  = document.querySelector('.message');
+
+   //asign min and max
+  
+
+   minNum.textContent = min;
+   maxNum.textContent= max;
+   //play again event listner
+   game.addEventListener('mousedown' ,function (e){
+    if (e.target.className === 'play-again'){
+      window.location.reload();
+    }
+  })
+
+
+   //listten for guess
+   guessbtn.addEventListener('click',function(){
+     let guess = parseInt(guessInput.value);
+
+     //validate
+     if(isNaN(guess) || guess < min || guess > max){
+       setMessage ('please  enter the no btw '  + min + ' and ' + max ,'red');
+     }
+     //check if won 
+     if(guess === winningNUM){
+       //game over - won
+       gameover(true, winningNUM + ' Is Correct You Win')
+      //  //disable input
+      //  guessInput.disabled =true;
+      //  //change border green 
+      //  guessInput.style.borderColor ='green';
+       
+      //  //set message 
+      //  setMessage(winningNUM + ' Is Correct You Win', 'green');
+
+
+     }
+     else {
+       //wrong nmber 
+       guessleft -=1;
+       if(guessleft === 0){
+         //game over - lost
+         gameover(false,  ' Game Over ,You Lost .The correct no is  ' + winningNUM)
+          //disable input
+      //  guessInput.disabled =true;
+      //  //change border green 
+      //  guessInput.style.borderColor ='red';
+       
+      //  //set message 
+      //  setMessage( ' Game Over ,You Lost .The correct no is  ' + winningNUM , 'red');
+
+
+       }else {
+         //change border color
+       guessInput.style.borderColor ='red';
+
+       //clear input 
+       guessInput.value = '';
+         //game constinous - ans wrong
+
+         setMessage(guess +' is not correct, ' + guessleft + ' guessleft','red');
+       }
+
+     }
+  
+
+   });
+
+   //game over
+   function gameover(won,msg) {
+
+    let color;
+    won === true ? color ='green' : color ='red';
+      //disable input
+      guessInput.disabled =true;
+      //change border green 
+      guessInput.style.borderColor =color;
+      //swt text color
+      message.style.color =color;
+      //set message 
+      setMessage(msg);
+
+     //play again?
+     guessbtn.value ='Play Again';
+     guessbtn.className +='play-again';
+   }
+/// GET WINNING NUMBER
+ function getRandomNUM( max  , min) {
+  //  return Math.floor(Math.random()*(max-min+1)+min));
+ }
+   //set message
+   function setMessage(msg,color){
+     message.style.color = color;
+
+     message.textContent=msg;
+   }
+
+
